c7f5237b11d2fe405c3a9230708b066b558b9f6d
carol faro <caroline.faro@gmail.com>
2022-02-03 18:41:16 -0300

    add 5 ex

--- fundamentos/bloco4_js/1_primeirospassos/scripts.js
+++ fundamentos/bloco4_js/1_primeirospassos/scripts.js
@@ -1,3 +1,6 @@
+/*
+exercicio 4
+
 const a = 8;
 const b = 2;
 const c = 9;
@@ -10,4 +13,21 @@ if (a >= 0) {
     console.log("positivo");
 } else {
     console.log("negative")
+};*/
+
+/*
+exercicio 5
+
+const angulo1 = 100;
+const angulo2 = 30;
+const angulo3 = 50;
+let angulo = 180;
+let resultado = angulo1 + angulo2 + angulo3;
+
+if (resultado === angulo) {
+    console.log(resultado == angulo)
+} else {
+    console.log(!resultado === angulo)
 };
+*/
+
