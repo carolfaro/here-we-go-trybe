ee3ec63c511c6b9c67c5ae2faf7e2f64b611bddb
carol faro <caroline.faro@gmail.com>
2022-02-04 21:08:32 -0300

    add 4 exercise

--- fundamentos/bloco4_js/2_arrayfor/script.js
+++ fundamentos/bloco4_js/2_arrayfor/script.js
@@ -18,11 +18,28 @@
 
 // console.log(soma);
 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index +=1) {
+// soma = soma + numbers[index];
+// }
+
+// console.log(soma / numbers.length);
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0
 
 for (let index = 0; index < numbers.length; index +=1) {
 soma = soma + numbers[index];
 }
 
-console.log(soma / numbers.length);
+media = (soma / numbers.length);
+
+if (media > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor que 20")
+}
+
