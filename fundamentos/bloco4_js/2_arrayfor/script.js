a03ee44275b1a184abb34450cc635fd6eacce8f2
carol faro <caroline.faro@gmail.com>
2022-02-08 21:47:30 -0300

    add 6 and 7 exercise

--- fundamentos/bloco4_js/2_arrayfor/script.js
+++ fundamentos/bloco4_js/2_arrayfor/script.js
@@ -260,24 +260,63 @@ let arrayPrimos = [];
 //       console.log(info[key])
 //   }
 
-  let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
+//   let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   };
+
+//   let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+//   };
+
+//  for (let index in info) {
+//   if (info[index] === info2[index]) {
+//       console.log('igual')
+//   } else {
+//       console.log(info[index] + " e " + info2[index])
+//   }
+// }
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+
+//   console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
+
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-  };
+  leitor.livrosFavoritos.push(
+      {
 
- for (let index in info) {
-  if (info[index] === info2[index]) {
-      console.log('igual')
-  } else {
-      console.log(info[index] + " e " + info2[index])
-  }
-}
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+      }
+  )
+
+  console.log(leitor);
