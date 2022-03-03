29b368cfc4030e0b0491cfdc4abde93e5cff7f5d
carol faro <caroline.faro@gmail.com>
2022-03-03 19:46:14 -0300

    add exercises of the day

---
+++ fundamentos/bloco7_jsES6/script.js
@@ -0,0 +1,50 @@
+// PARTE 1
+// 1. 
+
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+
+  const testingScope = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(`${ifScope} ' o que estou fazendo aqui ? :O'`);
+}
+
+  testingScope(true);
+
+  // 2.
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const sortNumbers = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+    
+  }
+ 
+  const ordenados = sortNumbers();
+
+ console.log(`os números ficaram assim ${ordenados}`);
