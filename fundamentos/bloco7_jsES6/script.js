ae7059ead6479407a62614b39d620775a151eeaa
carol faro <caroline.faro@gmail.com>
2022-03-03 20:56:06 -0300

    add part of part 2

--- fundamentos/bloco7_jsES6/script.js
+++ fundamentos/bloco7_jsES6/script.js
@@ -16,35 +16,66 @@
 //   testingScope(true);
 
 
-  const testingScope = (escopo) => {
-    if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    console.log(`${ifScope} ' o que estou fazendo aqui ? :O'`);
-}
+//   const testingScope = (escopo) => {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(`${ifScope} ' o que estou fazendo aqui ? :O'`);
+// }
 
-  testingScope(true);
+//   testingScope(true);
 
-  // 2.
-  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+//   // 2.
+//   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-  const sortNumbers = () => {
-    oddsAndEvens[0] = 2;
-    oddsAndEvens[1] = 3;
-    oddsAndEvens[2] = 4;
-    oddsAndEvens[3] = 7;
-    oddsAndEvens[4] = 10;
-    oddsAndEvens[5] = 13;
+//   const sortNumbers = () => {
+//     oddsAndEvens[0] = 2;
+//     oddsAndEvens[1] = 3;
+//     oddsAndEvens[2] = 4;
+//     oddsAndEvens[3] = 7;
+//     oddsAndEvens[4] = 10;
+//     oddsAndEvens[5] = 13;
 
-    return oddsAndEvens;
+//     return oddsAndEvens;
     
-  }
+//   }
  
-  const ordenados = sortNumbers();
+//   const ordenados = sortNumbers();
 
- console.log(`os números ficaram assim ${ordenados}`);
+//  console.log(`os números ficaram assim ${ordenados}`);
+
+//  // PARTE 2
+//  // 1.
+
+//  const fatorial = (param1) => {
+//      let resultado = 1;
+//      for (let index = 2; index <= param1; index += 1) {
+//      resultado *= index;
+//      }
+//      return resultado;
+//  };
+
+//  console.log(fatorial(5));
+
+ //recursivo
+
+//  const fatorial = param1 => param1 > 1 ? param1 * fatorial(param1 - 1) : 1;
+
+//  console.log(fatorial(5));
+
+ //2.
+
+ const maiorPalavra = palavra => {
+    let separaPalavra = palavra.split(' ');
+    if (separaPalavra[0] > separaPalavra[1]) {
+        console.log("e")
+    }
+     return separaPalavra
+ }
+
+ console.log(maiorPalavra("oi carol linda"));
