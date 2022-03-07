ba093bdd31c621cf8a949bb245e494718b6f4e91
carol faro <caroline.faro@gmail.com>
2022-03-07 20:55:28 -0300

    trying exercise 1

--- new_project/sum.test.js
+++ new_project/sum.test.js
@@ -1,1 +1,9 @@
-function sum(1, b)
+const sum = require('./sum');
+
+test('Espera que o retorno de soma 4 e 5 retorne 9', () => {
+    expect(sum(4, 5)).toBe(9);
+});
+
+test('Espera que o retorno de soma 0 e 0 retorne 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
