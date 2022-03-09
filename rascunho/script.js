ee9d9616060356103fccf441a6cb37fe23bba86f
carol faro <caroline.faro@gmail.com>
2022-03-09 07:37:11 -0300

    add function with some condictions

--- rascunho/script.js
+++ rascunho/script.js
@@ -7,3 +7,62 @@ const somaA = (numeros1) => {
     }
     return somaB
 };
+
+
+// estrutura de um teste
+
+describe('Descreve o que a função vai testar', (nomedafuncao)=> {
+    it('qual teste a função vai realizar tipo: soma 2 + 2 e recebe 4 de retorno', () => {
+        expect(nomedafuncao(2, 2)).toBe(4);
+      });
+    });
+
+// compara tipo
+
+if(typeof value !== 'number'){};
+
+// TESTE PROJETO
+
+
+const somaC = (numbers) => {
+    let result = 0;
+    let valor = numbers.lenght;
+    for (let index = 0; index < numbers.length; index += 1) {
+      if (typeof numbers[index] !== 'number' || valor === 0) {
+        result = 'undefined';
+      } else {
+          result = 'deu certo'
+      }
+    }
+    return result;
+};
+console.log(somaC([]));
+
+
+
+const somaD = (numbers) => {
+    let valor = numbers;
+    if (valor.length === 0) {
+        return 'consegui'
+    } else {
+        return 'de novo';
+    }
+};
+console.log(somaD([1]));
+
+// função com várias condicionais, uma antes da outra:
+
+const average = (numbers) => {
+    if (numbers.length === 0) {
+      return undefined;
+    }
+    for (let num in numbers) {
+      if (typeof numbers[num] !== 'number') {
+        return undefined;
+      }
+    }
+    const sum = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+    return Math.round(sum);
+  };
+
+  // uma condicional por vez.
