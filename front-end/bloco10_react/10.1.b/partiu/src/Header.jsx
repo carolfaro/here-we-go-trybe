e5496466f195f0b5b10dc865499b27cffa7f61c9
carol faro <caroline.faro@gmail.com>
2022-04-05 12:03:36 -0300

    finish exercises 10.1

---
+++ front-end/bloco10_react/10.1.b/partiu/src/Header.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+// importa somente o bloco Component de React
+
+class Header extends Component {
+    render() {
+        return <h1>Conteúdos de Front-End</h1>
+    }
+}
+
+export default Header
