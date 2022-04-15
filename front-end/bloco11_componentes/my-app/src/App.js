ddb0ca86680265b1707b10e41daece8101c6a6f8
carol faro <caroline.faro@gmail.com>
2022-04-15 17:39:57 -0300

    studing 11.1

---
+++ front-end/bloco11_componentes/my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Cores from './components/Cores'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>meu componente de cores</h1>
+      </header>
+      <Cores />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/bloco11_componentes/my-app/src/components/Cores.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Cores.css'
+import colors from '../colors'
+
+class Cores extends React.Component {
+    // OU 
+    // state = {filtro: 'xxx'}
+    // fora do constructor
+    
+    constructor(props){
+        super(props)
+        this.state = {filtro: 'xxx'}
+    }
+
+    handleChange(){
+
+    }
+
+    render(){
+        return(
+        <div>
+            <div>
+                <hr/>
+                <h2>Minhas cores</h2>
+                <h3>Meu filtro é {this.state.filtro}</h3>
+                <input type="text" value="" onChange={(e) => console.log(e.target.value)} />
+            </div>
+            <div>
+                <ul>
+                    {colors.map((element) => <li key={element.name}>{element.name}</li>)}
+                </ul>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Cores;
