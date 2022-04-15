7063a36457fca8ffbf5d3a5285af10b695450afc
carol faro <caroline.faro@gmail.com>
2022-04-15 19:01:31 -0300

    add some css

--- front-end/bloco11_componentes/my-app/src/components/Cores.js
+++ front-end/bloco11_componentes/my-app/src/components/Cores.js
@@ -22,20 +22,29 @@ class Cores extends React.Component {
     }
 
     render(){
-        // onChange={(e) => this.handleChange(e)}
-        // onChange={this.handleChange}
+        // onChange={(e) => this.handleChange(e)} -> sem o bind
+        // onChange={this.handleChange} -> com o bind
+
+        // filter e map
+        // map coloca as cores nas lis
+        // o filter faz uma verificação: 
+        // se o estado do filtro for string vazia (estado do filtro = valor input) mostra todos os elementos
+        // ou se tiver alguma coisa, vc filtra tudo q tem aquela coisa dentro do estado do filtro
+
         return(
         <div>
             <div>
                 <hr/>
-                <h2>Minhas cores</h2>
+                <h2 className="titulo">Minhas cores</h2>
                 <h3>Meu filtro é {this.state.filtro}</h3>
                 <input type="text" value={this.state.filtro} 
                 onChange={this.handleChange} />
             </div>
             <div>
-                <ul>
-                    {colors.map((element) => <li key={element.name}>{element.name}</li>)}
+                <ul className="ul1">
+                    {colors
+                    .filter((ele) => this.state.filtro === '' || ele.name.includes(this.state.filtro))
+                    .map((element) => <li id={element.name}key={element.name}>{element.name}</li>)}
                 </ul>
             </div>
         </div>
