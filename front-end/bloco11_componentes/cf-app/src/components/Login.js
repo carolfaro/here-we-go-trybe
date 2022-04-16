09611c3b05ecf62f79d4023c8b0ba490b3b1bdc4
carol faro <caroline.faro@gmail.com>
2022-04-16 09:18:01 -0300

    definystate

--- front-end/bloco11_componentes/cf-app/src/components/Login.js
+++ front-end/bloco11_componentes/cf-app/src/components/Login.js
@@ -2,14 +2,32 @@ import React from 'react'
 
 class Login extends React.Component {
     
+    constructor(){
+        super()
+        this.state ={
+            email: 'oi?',
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e){
+        const { value } = e.target;
+        this.setState({
+            email: value,
+        })
+
+    }
+
     render() {
+        const { email } = this.state;
+
         return (
         <section>
             <h2>Login</h2>
             <form>
                 <label htmlFor='email'>
-                    email
-                    <input name="email" value="" onChange={() => console.log('ta usando o input')} />
+                    email 
+                    <input name={ email} value="" onChange={ this.handleChange } />
                 </label>
             </form>
         </section>)
